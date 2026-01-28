1521cdad3fc8c55657b7fe134798140761651128
Arjun-Bhattarai <arjunbhattarai860@gmail.com>
2026-01-28 19:43:50 +0545

    GET-Request using postman

--- test2.py
+++ test2.py
@@ -6,13 +6,25 @@ app = Flask(__name__)
 def get_user(user_id):
     user_data ={
         "user-id":user_id,
-        "name":"John Doe",  
+        "username":"Arjun",  
         "email":"john.doe@example.com"
     }
 
     extra=request.args.get("extra")
     if extra:
         user_data["extra"]=extra
-        return jsonify(user_data),200
+    return jsonify(user_data),200
+
+
+@app.route("/create-user/", methods=["POST"])
+def create_user():
+    print("Creating a new user")
+    try:
+        data = request.get_json()
+    except Exception as e:
+        return jsonify({"error": "Invalid JSON"}), 400
+    print("Received data:", data)
+    return jsonify(data)  ,201
+
 if __name__ == "__main__":
     app.run(debug=True)
